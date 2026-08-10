c2955a94aeb81e7ca420a4a36ecd738716d09ec0
tmyroy <tmyroy@proton.me>
2026-08-10 17:29:01 +0200

    feat(ui): add clickable side menu, draft cards and grid controls
    refs #23

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import Phaser from "phaser";
-import { CELL_SIZE, GRID_HEIGHT, GRID_WIDTH } from "./grid";
-import { DETAILS_PANEL_HEIGHT, MainScene } from "./scenes/MainScene";
+import { CANVAS_HEIGHT, CANVAS_WIDTH, MainScene } from "./scenes/MainScene";
 
 new Phaser.Game({
   type: Phaser.AUTO,
   parent: "app",
-  width: GRID_WIDTH * CELL_SIZE,
-  height: GRID_HEIGHT * CELL_SIZE + DETAILS_PANEL_HEIGHT,
+  width: CANVAS_WIDTH,
+  height: CANVAS_HEIGHT,
   backgroundColor: "#1d1d1d",
   scene: [MainScene],
 });
